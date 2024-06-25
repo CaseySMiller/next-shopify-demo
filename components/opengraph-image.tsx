40e624b96f61c217dbdbd95d4d93e0260d92cb09
CaseySMiller <CaseySMiller@yahoo.com>
2024-06-25 06:13:25 -0600

    fixed type error in opengraph-image.tsx

--- components/opengraph-image.tsx
+++ components/opengraph-image.tsx
@@ -1,5 +1,7 @@
+import Image from 'next/image';
 import { ImageResponse } from 'next/og';
-import LogoIcon from './icons/icon-512.png';
+// import LogoIcon from './icons/icon-512.png';
+import Logo from './icons/icon-192.png';
 
 export type Props = {
   title?: string;
@@ -17,7 +19,9 @@ export default async function OpengraphImage(props?: Props): Promise<ImageRespon
     (
       <div tw="flex h-full w-full flex-col items-center justify-center bg-black">
         <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
-          <LogoIcon width="64" height="58" fill="white" />
+          {/* <LogoIcon width="64" height="58" fill="white" /> */}
+          <Image src={Logo} alt={ `${title} logo` } width="64" height="58" />
+
         </div>
         <p tw="mt-12 text-6xl font-bold text-white">{title}</p>
       </div>
